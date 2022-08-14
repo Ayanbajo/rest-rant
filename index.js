c7b1cefd587c302093b5772bcc402e9e081a207d
ayanbajo <bolatitoayanbajo@gmail.com>
2022-08-14 11:57:50 -0400

    project started

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.get('/', function (req, res) {
+    // This gets sent to the client 
+    // (your web browser most likely!)
+    res.send('Hello world')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1> 404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
