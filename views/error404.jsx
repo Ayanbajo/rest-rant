04b3fe19512a76196fb4cc3636dfdc900780a484
ayanbajo <bolatitoayanbajo@gmail.com>
2022-08-18 19:59:10 -0400

    Adding stles to Rest-Rant

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+              <img src="/images/icecream.jpg" alt="icecream"/>
+              </div>
+              <div>
+              Photo by <a href="https://unsplash.com/@victoriakosmo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Victoria Shes</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
           </main>
       </Def>
     )
