b9ab99320621c202174ec654cda49ace3844c928
ayanbajo <bolatitoayanbajo@gmail.com>
2022-08-19 22:35:52 -0400

    New places can be added

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ app.use('/places', require('./controllers/places'))
 app.get('/', (req, res) => {
     res.render('home')
   })
-  
-// app.get('*', (req, res) => {
-//     res.status(404).send('<h1> 404 Page</h1>')
-// })
 
 app.get('*', (req, res) => {
     res.render('error404')
