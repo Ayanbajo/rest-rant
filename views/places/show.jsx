63b562ad8b8ef913a14a4b3a7959a7e06976b605
ayanbajo <bolatitoayanbajo@gmail.com>
2022-08-21 11:01:20 -0400

    Delete functinality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,17 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <img src={data.place.pic} alt={data.place.name} />
+            <h2>Rating</h2>
+            <p>
+                Not rated
+            </p>
+            <h2>Descritption</h2>
+            <p>
+            Located in {data.place.city}, {data.place.state}
+            </p>
+            <h2>Comments</h2>
+            <p>No Comments yet!</p>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>     
             <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
                 <button type="submit" className="btn btn-danger">Delete</button>
