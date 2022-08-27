e226a2c0c9eb73c59f66c2c3684773d343f47c05
ayanbajo <bolatitoayanbajo@gmail.com>
2022-08-26 23:08:10 -0400

    Added mongoose

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -9,28 +9,28 @@ function edit_form (data) {
           </main>
             <form method="POST" action={`/places/${data.id}?_method=PUT`}>
                 <div className="row">
-                <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                <div className="col-sm-6 col-md-4 col-lg-3">
                     <label htmlFor="name">Place Name</label>
                     <input className="form-control" id="name" name="name" value={data.place.name} required />
                 </div>
-                <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                <div className="col-sm-6 col-md-4 col-lg-3">
                     <label htmlFor="pic">Place Picture</label>
-                    <input className="form-control" id="pic" name="pic" value={data.place.pic} required />
+                    <input className="form-control" id="pic" name="pic" value={data.place.pic} />
                 </div>
-                <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                <div className="col-sm-6 col-md-4 col-lg-3">
                     <label htmlFor="pic">City</label>
-                    <input className="form-control" id="city" name="city" value={data.place.city} required />
+                    <input className="form-control" id="city" name="city" value={data.place.city} />
                 </div>
-                <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                <div className="col-sm-6 col-md-4 col-lg-3">
                     <label htmlFor="pic">State</label>
-                    <input className="form-control" id="state" name="state" value={data.place.state} required />
+                    <input className="form-control" id="state" name="state" value={data.place.state} />
                 </div>
-                <div className="form-group col-sm-6 col-md-4 col-lg-3">
+                <div className="col-sm-6 col-md-4 col-lg-3">
                     <label htmlFor="pic">Cuisines</label>
                     <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
                 </div>
                 </div>
-                <button className="btn btn-primary">Add places</button>
+                <button className="btn btn-secondary">Add places</button>
             </form>
         </Def>
     )
